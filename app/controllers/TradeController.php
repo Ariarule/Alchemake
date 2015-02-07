47da63ab23660484db72a7a66b0b8b08ce363ed7
Nic Smith <nic@ariarule.com>
2015-02-07 11:33:54 -0600

    More work on Trade proposeAction

--- app/controllers/TradeController.php
+++ app/controllers/TradeController.php
@@ -1,29 +1,6 @@
 <?php
 class TradeController extends AlchemakeController {
 
-private function is_logged_in_user_proposer($proposer) {
-  if ($proposer === $this->userThatIsLoggedIn()->userid) {
-    return true;
-    }
-  return false;
-  }
-  
-private function clean_trade($unclean_trade) {
-  array_walk_recursive($unclean_trade,'clean_trade_element');
-
-  if (!does_user_exist($unclean_trade['proposed']['userid'])) {
-    trigger_error("Trading partner is not present in Alchemake. :(",E_USER_ERROR);
-    }
-  if (!does_user_exist($unclean_trade['proposer']['userid'])) {
-    trigger_error("Trading partner is not present in Alchemake. :(",E_USER_ERROR);
-    }
-  if (!is_logged_in_user_proposer($unclean_trade['proposer']['userid'])) {
-    trigger_error("Technical problem: cannot make trade. Sorry. :(",E_USER_ERROR);
-    }
-
-  return $unclean_trade; //the trade is now clean
-  }
-
  private function cleanItems($dirty_items) {
      $clean = [];
      foreach ($dirty_items as $item_id => $qty) {
@@ -70,6 +47,21 @@ private function userQtyShorts($user,$proposals) {
     return $problems;
 }
 
+private function addDetailToTrade($tradeid,$direction,$itemid,$qty) {
+    $trade_detail = new TradeDetails();
+    $trade_detail->tradeid = $tradeid;
+    $trade_detail->itemid = $itemid;
+    $trade_detail->direction = $direction;
+    $trade_detail->qty = $qty;
+    return $trade_detail->save();
+}
+
+private function saveTradeDetails($tradeid,$item_details,$direction) {
+    foreach ($item_details as $itemno => $qty) {
+        $this->addDetailToTrade($tradeid,$direction,$itemno,$qty);
+    }
+}
+
 public function proposeAction() {
     $posted_trade = $this->request->getPost();
     $trade = new Trades();
@@ -98,68 +90,10 @@ public function proposeAction() {
             'action'=>'index'));
     }
     
+    $trade->save();
+    $this->saveTradeDetails($trade->tradeid, $proposed_items_info,'TO_PROPOSER');
+    $this->saveTradeDetails($trade->tradeid, $proposer_items_info,'FROM_PROPOSER');
 }
-    
-    
-
-    
-}  
-
-
-
-
-//  $trade_error = FALSE;
-//  foreach($trade['proposer']['items'] as $itemid => $proposer_table_qty) {
-//    if (!isset($reformatted_inventory[$itemid]['available'])) {
-//      $reformatted_inventory[$itemid] = array();
-//      $reformatted_inventory[$itemid]['available'] = 0;
-//      }
-//    if ($proposer_table_qty > $reformatted_inventory[$itemid]['available']) {
-//      $trade_error = TRUE;
-//      echo "You have suggested trading {$proposer_table_qty}x {$reformatted_inventory[$itemid]['name']} but you have only {$reformatted_inventory[$itemid]['available']} available. <br />";
-//      }
-//    }
-//  if ($trade_error) {
-//    trigger_error("Cannot suggest this trade -- it's not possible to trade a larger quantity of something than you currently have available.",E_USER_ERROR);
-//    }
-//
-//  $sql = "INSERT INTO `trades` (`proposer_userid`,`proposed_userid`,`status`) VALUES ('{$trade['proposer']['userid']}','{$trade['proposed']['userid']}','pending');";
-//  $insert_link = mysql_query($sql,$mysql_link);
-//  if ($insert_link) {
-//    $tradeid = mysql_insert_id($mysql_link);
-//    }
-//  else {
-//    trigger_error($general_oops,E_USER_ERROR);
-//    //echo $sql . " gave " . mysql_error() . "\n";
-//    }
-//
-//  $trade_detail_rows = max(sizeof($trade['proposer']['items']),sizeof($trade['proposed']['items']));
-//  $proposer_item_keys = array_keys($trade['proposer']['items']);
-//  $proposed_item_keys = array_keys($trade['proposed']['items']);
-//
-//
-//  for ($i = 0; $i < $trade_detail_rows; $i++) {
-//    if (isset($proposer_item_keys[$i])) {
-//      $proposer_item_qty = $trade['proposer']['items'][$proposer_item_keys[$i]];
-//      $proposer_item = $proposer_item_keys[$i];
-//      }
-//    else {
-//      $proposer_item_qty = '0';
-//      $proposer_item = '0';
-//      }
-//    if (isset($proposed_item_keys[$i])) {
-//      $proposed_item_qty = $trade['proposed']['items'][$proposed_item_keys[$i]];
-//      $proposed_item = $proposed_item_keys[$i];
-//      }
-//    else {
-//      $proposed_item_qty = '0';
-//      $proposed_item = '0';
-//      }
-//    //FUTURE: COMBINE INTO SINGLE QUERY!
-//    $sql = "INSERT INTO `alchemake`.`tradedetails` (`tradeid`, `proposer_itemid`, `proposer_qty`, `proposed_itemid`, `proposed_qty`) VALUES ('$tradeid', '$proposer_item', '$proposer_item_qty', '$proposed_item', '$proposed_item_qty');";
-//    $insert_link = mysql_query($sql,$mysql_link);
-//    }
-//}
 
 public function rejectAction() {
   //TODO: Rewrite
@@ -178,7 +112,7 @@ public function rejectAction() {
     }
   }
 
-public function withdraw() {
+public function withdrawAction() {
   //TODO: Rewrite
   if ($user_role & PROPOSER) {
     $sql = "UPDATE `alchemake`.`trades` SET `status` = 'withdrawn' WHERE `trades`.`tradeid` ={$_GET['tradeid']} LIMIT 1 ;";
@@ -194,7 +128,9 @@ public function withdraw() {
     trigger_error("Only the person that made this offer can reject it.",E_USER_ERROR);
     }
   }
-public function accept() {
+  
+  
+public function acceptAction() {
   //TODO: Rewrite
   if ($user_role & PROPOSED) {
     $sql = "SELECT * FROM `tradedetails` WHERE `tradeid` = {$_GET['tradeid']}";
