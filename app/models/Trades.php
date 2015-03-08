28f74381dae219f0c072330c805de31f6b91a4b2
Nic Smith <nic@ariarule.com>
2015-03-08 00:52:06 -0600

    Add static methods to Trades to find trades that can be accepted.
    * Add findAcceptable()
    * Add isAcceptable()
    * Add findAcceptableByUserId

--- app/models/Trades.php
+++ app/models/Trades.php
@@ -1,4 +1,6 @@
 <?php
+use Phalcon\Mvc\Model\Resultset\Simple as Resultset;
+
 class Trades extends Phalcon\Mvc\Model  {
 
   public $status;
@@ -10,13 +12,50 @@ class Trades extends Phalcon\Mvc\Model  {
   public function initialize() {
     $this->skipAttributes(['timestamp']);
     $this->hasMany('tradeid','TradeDetails','tradeid');
-    $this->belongsTo('proposer_userid','Users','userid');
-    $this->belongsTo('proposed_userid','Users','userid');
+    $this->belongsTo('proposer_userid','Users','userid',['alias' => 'proposer']);
+    $this->belongsTo('proposed_userid','Users','userid',['alias' => 'proposed']);
   }
   
-  public function acceptable() {
-      return ((time() - strtotime($this->timestamp)) < 7776000
-        && $this->status === 'proposed');
+  public function isAcceptable() {
+      return @strtotime($timestamp) < 2678400;
+      //this intentionally has a slight grace period on it
+      //compared to findAcceptable
+      //the intention is that a trade is available for action for 
+      //a while after its been viewed
+      //I think the original had something like this.
   }
+  
+  public static function findAcceptable($conditions = NULL) {
+      //because we are using raw MySQL style SQL here,
+      //public functions must validate their added conditions
+      $trade = new Trades();
+      $sql = "select * from trades where status = 'pending' "
+            . "AND TIMESTAMPDIFF(DAY,timestamp,now()) < 30 ";
+      if ($conditions) {
+          $sql .= " AND $conditions ";
+      }
+      return new Resultset(null,
+              $trade,
+              $trade->getReadConnection()->query($sql));
+  }
+  
+  public static function findAcceptableByUserId($userid,$field) {
+      $userid = (int)$userid;
+      if ($field === 'both') {
+          $fields = ['proposer','proposed'];
+      }
+      elseif (($field === 'proposer') || ($field === 'proposed')) {
+          $fields = [$field];
+      }
+      
+      foreach ($fields as &$query_field) {
+          $query_field = " {$query_field}_userid = $userid";
+      }
+      $query = implode(" AND ", $fields);
+
+      return self::findAcceptable($query);
+  }
+  
+  
   
 }
