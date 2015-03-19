2e62da1dce39d3d35f77195cd21c3270080678c3
Nic Smith <nic@ariarule.com>
2015-03-18 21:36:30 -0500

    Rm item_names function
    The item name is accessible by the child item where it is needed.

--- app/models/Items.php
+++ app/models/Items.php
@@ -28,17 +28,6 @@ class Items extends Phalcon\Mvc\Model {
     $this->belongsTo('itemid','TradeDetails','itemid');
   }
 
-  function item_names(&$items) {
-    global $mysql_link;
-    //UGLY HACK: Loads all item names into memory for trade tables. Should really be done in MySQL with multiple joins
-    //OTOH: Can use for real-time checking of possible combinations, etc.
-    $sql = "SELECT * FROM `items`;";
-    $items_r = mysql_query($sql,$mysql_link);
-    while ($c_item = mysql_fetch_assoc($items_r)) {
-      $items[$c_item['itemid']] = $c_item;
-      }
-    }
-
   function isItemBasic($itemno) {
     //possible improvement: mysql call?
     return (($itemno > 15) && ($itemno < 26));
